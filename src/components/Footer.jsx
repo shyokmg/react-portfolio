161a5fa4129dc1481978d8c27299455d2788d827
Shoyo Kumagai <shyokmg.au@gmail.com>
2024-01-14 12:30:19 +1100

    Fixed alignment of links in footer component:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,26 @@
 import { SiGithub } from "react-icons/si";
-import { AiOutlineLinkedin } from "react-icons/ai";
+import { GrLinkedin } from "react-icons/gr";
+import { ImStackoverflow } from "react-icons/im";
 
 export default function Footer() {
   const githubLink = "https://github.com/shyokmg";
   const linkedInLink = "https://www.linkedin.com/in/shoyo-kumagai-855020123/";
+  const stackOverFlowLink = "https://stackoverflow.com/"
   return (
-    <div>
+    <div className="d-flex justify-content-center gap-3">
       <h1>
         <a href={githubLink} target="_blank" rel="noopener noreferrer">
           <SiGithub />
         </a>
+      </h1>
+      <h1>
         <a href={linkedInLink} target="_blank" rel="noopener noreferrer">
-          <AiOutlineLinkedin />
+          <GrLinkedin />
+        </a>
+      </h1>
+      <h1>
+        <a href={stackOverFlowLink} target="_blank" rel="noopener noreferrer">
+        <ImStackoverflow />
         </a>
       </h1>
     </div>
